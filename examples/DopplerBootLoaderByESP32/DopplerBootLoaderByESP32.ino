0715f20848fff6598b742cf5fa850237d2689568
Sven Braun <me@no-scene.de>
2018-10-24 11:50:58 +0200

    Update examples/DopplerBootLoaderByESP32/DopplerBootLoaderByESP32.ino
    update some comments

--- examples/DopplerBootLoaderByESP32/DopplerBootLoaderByESP32.ino
+++ examples/DopplerBootLoaderByESP32/DopplerBootLoaderByESP32.ino
@@ -1,32 +1,31 @@
-/*
- * 
- * DOPPLER-Board-Layout: 
- *                                                                                    ---------------- FPGA Pins ------------------
- *                                                                                   LedR LedG LedB
- * DIL Pin 48   47   46   45   44   43   42   41   40   39   38   37   36   35   34   33   32   31   30   29   28   27   26   25
- *       |--O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O---|
- * name  | VIN  5V  3.3V PA11 PA10 PA09 PA08 PA07 PA06 PA05 PA04 PB09 PB08 PA02  GND  F41  F40  F39  F38  F37  F36  F35  F34  F32  |
- *       |                                                                                            ö  ö  ö  ö                   |
- *      |                                                                                             ö  ö  ö  ö         |BTN:S1|  |
- *     | USB                           DOPPLER: SamD51 <- SPI -> icE40        |BTN:RESET|             ö  ö  ö  ö                   |
- *      |                                                                                             ö  ö  ö  ö         |BTN:S2|  |
- *       |                                                                                                                         |
- * name  | GND PA13 PA12 PB11 PB14 PA15 PB10 PA31 PA30  RES PA19 PA20 PA21 PA22 3.3V  F11  F12  F13  F18  F19  F20  F21  F23  F25  |
- *       L--O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O---|
- * DIL Pin  1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24
- *             SCL  SDA                       SWD  SWC RES
- *             -- I2C--                       --- SWD  ---   ----- Shared  -----      ---------------- FPGA Pins ------------------
- */
+   /*          DOPPLER-Board-Layout:
+     *                                                                                    ---------------- FPGA Pins ------------------
+     *                                                     DAC1      SCK  MOSI DAC0      LedR LedG LedB       CT1            CP0
+     * DIL Pin 48   47   46   45   44   43   42   41   40   39   38   37   36   35   34   33   32   31   30   29   28   27   26   25
+     *       |--O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O---|
+     * name  | VIN  5V  3.3V  A10  A9   A8   A7   A6   A5   A4   A3   A2   A1   A0   GND  R2   R1   R0   F14  F13  F12  F11  F10  F9   |
+     * alt   | VIN  5V  3.3V PA11 PA10 PA09 PA08 PA07 PA06 PA05 PA04 PB09 PB08 PA02  GND  41   40   39   38   37   36   35   34   32   |
+     *       |                                                                                            ö  ö  ö  ö                   |
+     *      |                                                                                             ö  ö  ö  ö         |BTN:S1|  |
+     *     | USB                           DOPPLER: SamD51 <- SPI -> icE40        |BTN:RESET|             ö  ö  ö  ö                   |
+     *      |                                                                                             ö  ö  ö  ö         |BTN:S2|  |
+     *       |                                                                                                                         |
+     * alt   | GND PA13 PA12 PB11 PA14 PA15 PB10 PA31 PA30  RES PA19 PA20 PA21 PA22 3.3V  11   12   13   18   19   20   21   23   25   |
+     * name  | GND   0    1    2    3    4    5                   6    7    8    9  3.3V  F0   F1   F2   F3   F4   F5   F6   F7   F8   |
+     *       L--O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O----O---|
+     * DIL Pin  1    2    3    4    5    6    7    8    9   10   11   12   13   14   15   16   17   18   19   20   21   22   23   24
+     *             SCL  SDA   MISO           SS   SWD  SWC RES                                 CT0                      CP0
+     *             -- I2C--                       --- SWD  ---   ----- Shared  -----      ---------------- FPGA Pins ------------------
+     */
 
 // Some useful Links
-// https://github.com/adafruit/Adafruit_DAP/blob/master/Adafruit_DAP.h
 // https://github.com/adafruit/circuitpython/issues/894
-// https://learn.adafruit.com/introducing-adafruit-itsybitsy-m4/pinouts
 
 #include "Adafruit_DAP.h"
+
+// How to create doppler.h
+// https://github.com/noscene/uf2-samdx1
 #include "doppler.h"
-
-
 
 // this Pins using with Node32S to flash Bootloader and Set Fuses
 #define SWDIO 5
@@ -109,7 +108,6 @@ void setup() {
   
 
   // Read CPU ID!!!
-
   Serial.print("\nCPU UniqueID (serial) 128bit : ");
   uint32_t rr = dap.dap_read_word(0x008061FC);
   Serial.print(rr,HEX);
@@ -321,7 +319,7 @@ Done!
   
   dap.fuseWrite();
   
-  Serial.println(" done.");
+  Serial.println("fuseWrite done.");
  
   // The SAMD51s bootloader protection can be temporarily disabled through an NVM
   rr = dap.dap_read_word(REG_NVMCTRL_CTRLB);
@@ -334,7 +332,7 @@ Done!
   dap.erase();
   Serial.println(" done.");
   
-  Serial.print("Programming... ");
+  Serial.print("Programming doppler bootlaoder... ");
 
   Serial.print(sizeof(binfile));
   Serial.print("\n");
@@ -348,7 +346,7 @@ Done!
   }
    Serial.println(addr);
   Serial.print(millis() - t);
-  Serial.println("Millis \nDone!");
+  Serial.println("Millis \nDone! DO this 2 Times!!!!!");
   dap.dap_set_clock(50);
 
   dap.deselect();
